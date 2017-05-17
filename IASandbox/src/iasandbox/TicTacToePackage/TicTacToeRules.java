00871cc57ee94f8d81ee58146a4dd0c0b8445004
VoiderX <gabrieldeabreu.ti@gmail.com>
2017-05-17 14:07:29 -0300

    Correção da Select ee TicTacToeRules

---
+++ IASandbox/src/iasandbox/TicTacToePackage/TicTacToeRules.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iasandbox.TicTacToePackage;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class TicTacToeRules implements ticTacToePlayer {
+
+    @Override
+    public int[] logic() {
+        return new int[]{0,0}; 
+    }
+    
+}
