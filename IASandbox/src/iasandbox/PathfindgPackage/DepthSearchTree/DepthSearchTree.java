3c64253a6649b2012ba850e8c7f099bd6f5a2a5f
VoiderX <gabrieldeabreu.ti@gmail.com>
2017-05-22 19:38:37 -0300

    Mayyybe encontrando todas as soluções funcionando

--- IASandbox/src/iasandbox/PathfindgPackage/DepthSearchTree/DepthSearchTree.java
+++ IASandbox/src/iasandbox/PathfindgPackage/DepthSearchTree/DepthSearchTree.java
@@ -16,6 +16,19 @@ import java.util.Iterator;
  */
 public class DepthSearchTree {
 
+    private ArrayList<NoDS> NosVisitados = new ArrayList<>();
+    private boolean solucaoEncontrada = false;
+
+    private boolean verificaNoVisitado(NoDS no) {
+        for (int i = 0; i < NosVisitados.size(); i++) {
+            if (no.getPos().getPos()[0] == NosVisitados.get(i).getPos().getPos()[0]
+                    && no.getPos().getPos()[1] == NosVisitados.get(i).getPos().getPos()[1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     private ArrayList<NoDS> geraFilhos(NoDS no) {
         ArrayList<NoDS> filhos = new ArrayList<>();
 
@@ -45,28 +58,38 @@ public class DepthSearchTree {
                 filhos.add(new NoDS(no.getPos().getUp()));
             }
         }
-        System.out.println("O no tem " + filhos.size() + "filhos");
-        return filhos;// Retorna os filhos do nó
+        //  System.out.println("O no tem " + filhos.size() + "filhos");
+        if (solucaoEncontrada == false) {
+            return filhos;// Retorna os filhos do nó
+        } else {
+            return new ArrayList<>();
+        }
     }
 
     private void geraArvore(NoDS no, int[] fim) {
-        if (!(no.getPos().getPos()[0] == fim[0] && no.getPos().getPos()[1] == fim[1])
-                || geraFilhos(no).isEmpty()) {
-            no.setFilhos(geraFilhos(no));
-            System.out.println("X=" + no.getPos().getPos()[0] + " Y=" + no.getPos().getPos()[1]);
-            Iterator<NoDS> it = no.getFilhos().iterator();
-            while (it.hasNext()) {
-                geraArvore(it.next(), fim);
+        if (verificaNoVisitado(no)) {
+            if (!(no.getPos().getPos()[0] == fim[0] && no.getPos().getPos()[1] == fim[1])) {
+                no.setFilhos(geraFilhos(no));
+                NosVisitados.add(no);
+                Iterator<NoDS> it = no.getFilhos().iterator();
+                while (it.hasNext()) {
+                    geraArvore(it.next(), fim);
+                }
+                System.out.println("X=" + no.getPos().getPos()[0] + " Y=" + no.getPos().getPos()[1]);
+            } else {
+                System.out.println("Solução X=" + no.getPos().getPos()[0] + " Y=" + no.getPos().getPos()[1]);
+                // solucaoEncontrada = true;
             }
+
         }
     }
 
     public static void main(String[] args) {
         GenMap obj = new GenMap();
-        int[][] layout = new int[2][2];
+        int[][] layout = new int[3][3];
         MapNode[][] map = obj.init(layout);
         NoDS arv = new NoDS(map[0][0]);
         DepthSearchTree dst = new DepthSearchTree();
-        dst.geraArvore(arv, new int[]{1, 0});
+        dst.geraArvore(arv, new int[]{2, 2});
     }
 }
